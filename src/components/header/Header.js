1475505a4a016a631c03d82ea3e485ceaf50cee2
farhan <farhansadikgalib@gmail.com>
2021-08-10 11:50:50 +0600

    init

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -70,7 +70,7 @@ function Header() {
             </li>
           )}
           <li>
-//            <a href="#contact">Contact Me</a>
+           {/* <a href="#contact">Contact Me</a> */}
           </li>
           <li>
             {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
